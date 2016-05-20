fff4a278c9f646c641745a8fa551e450a85773bb
GP <gp.gooiker@gmail.com>
2016-05-20 20:38:53 +0200

    Add a battle controller

---
+++ Assets/Scripts/BattleController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BattleController: MonoBehaviour {
+	// Attach to a player
+	public bool IsHittingLeft;
+	public bool IsHittingRight;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (0)) {
+			if (Input.mousePosition.x < 0.5 * Screen.width) {
+				IsHittingLeft = true;
+			} else {
+				IsHittingRight = true;
+			}
+		}
+	}
+}
