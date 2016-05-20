7161feb52d4ac745c1bb970b2611408322dff0a2
GP <gp.gooiker@gmail.com>
2016-05-20 19:58:52 +0200

    Click the left or right half of the screen to hit left or right

---
+++ Assets/Scripts/MovementController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovementController : MonoBehaviour {
+	// Attach to a player object
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (0)) {
+			if (Input.mousePosition.x < 0.5 * Screen.width) {
+				Debug.Log ("Hit left!");
+			} else {
+				Debug.Log ("Hit right!");
+			}
+		}
+	}
+}
