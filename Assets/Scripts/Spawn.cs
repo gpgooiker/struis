3e281d4e0d04246918df6335a4049be20118a319
wesley blijlevens <wesley.blijlevens@live.nl>
2016-05-21 11:42:42 +0200

    Basic spawn script for the enemy objects

---
+++ Assets/Scripts/Spawn.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawn : MonoBehaviour {
+    [SerializeField] private GameObject cactusPrefab;
+
+    public GameObject cactus;
+    Random rnd = new Random();
+
+    private int minHorizontal = -4;
+    private int maxHorizontal = 4;
+
+    IEnumerator SeedRandom()
+    {
+        yield return new WaitForSeconds(Random.Range(0, 100));
+    }
+
+    void SpawnObstacle()
+    {
+        cactus = Instantiate(cactusPrefab) as GameObject;
+        cactus.transform.position = new Vector3(Random.Range(minHorizontal, maxHorizontal), 14.63f, 0);
+        //make child of plane or spawn empty
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        StartCoroutine(SeedRandom());
+        SpawnObstacle();
+    }
+}
