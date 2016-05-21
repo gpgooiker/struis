8b65a5ad12d251722f13ac10af896aab548ab32e
GP <gp.gooiker@gmail.com>
2016-05-21 14:04:58 +0200

    Steering

--- Assets/Scripts/MovementController.cs
+++ Assets/Scripts/MovementController.cs
@@ -3,7 +3,8 @@ using System.Collections;
 
 [RequireComponent (typeof(AccelerometerInput))]
 
-public class MovementController : MonoBehaviour {
+public class MovementController : MonoBehaviour
+{
 	// Attach to a world object
 
 	public float WorldSpeed;
@@ -11,23 +12,29 @@ public class MovementController : MonoBehaviour {
 	private Transform worldTransform;
 	private float deltaY;
 	private Vector3 newPos;
+	private float turnSpeed = 3.0f;
+	private Vector3 playerLocation = new Vector3 (0, 1, 0);
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+	{
 		worldTransform = gameObject.GetComponent<Transform> ();
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+	{
+		float rotation = Input.GetAxis ("Horizontal") * turnSpeed;
+		worldTransform.RotateAround (playerLocation, Vector3.forward, rotation);
+
 
 		deltaY = worldTransform.position.y + WorldSpeed;
 		Debug.Log ("DeltaY" + deltaY);
 	
-		newPos = new Vector3(worldTransform.position.x, deltaY, worldTransform.position.z);
+		newPos = new Vector3 (worldTransform.position.x, deltaY, worldTransform.position.z);
 		worldTransform.position = newPos;
 
-		// Sturen:
-		// worldTransform.rotation.z += 2
+
 	}
 		
 }
